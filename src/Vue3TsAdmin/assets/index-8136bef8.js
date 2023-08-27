b1642d736269b92968c21405512dcf1626a69874
xfwl <3081370270@qq.com>
2023-08-27 18:06:22 +0800

    变更api请求路径

---
+++ src/Vue3TsAdmin/assets/index-8136bef8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index-ad306f01.js";const o={};function r(t,s){return c(),n("div",null,"这里是404")}const a=e(o,[["render",r]]);export{a as default};
---
+++ src/Vue3TsAdmin/assets/layoutSetting-97e847b9.js
@@ -0,0 +1,1 @@
+import{U as o,i as e}from"./index-ad306f01.js";const n=o("settingStore",()=>{const t=e(!1),r=e(!0);return{openAndFoldFlag:t,refreshFlag:r}});export{n as u};
